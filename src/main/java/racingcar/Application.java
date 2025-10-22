5d7c758051ce3e90b6522ecf60505766551e0fa0
xodyd123 <gksrmf9186@naver.com>
2025-10-22 18:26:14 +0900

    자바로 먼저 풀기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import racingcar.controller.UserController;
+import racingcar.view.ConsoleInputView;
+import racingcar.view.ConsoleOutView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        UserController userController = new UserController(new ConsoleOutView(), new ConsoleInputView());
+        userController.run();
     }
 }
